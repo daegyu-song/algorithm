34c2637d0415a4766019b65f321bff07a5414404
daegyu-song <sdk0109@naver.com>
2026-01-27 22:57:20 +0900

    programmers_level2_n2배열자르기

---
+++ programmers/level2/n2배열자르기.java
@@ -0,0 +1,14 @@
+package level2;
+
+public class n2배열자르기 {
+    public int[] solution(int n, long left, long right) {
+        int[] answer = new int[(int)(right - left) + 1];
+
+        int idx = 0;
+        for (long i = left; i <= right; i++) {
+            answer[idx++] = Math.max((int)(i / n), (int)(i % n)) + 1;
+        }
+
+        return answer;
+    }
+}

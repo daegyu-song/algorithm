6a0dc7985eb44ac67a12c57138909a06f8bfb26a
daegyu-song <sdk0109@naver.com>
2026-02-16 17:39:19 +0900

    swea_d4_햄스터

--- swea/d4/S_햄스터.java
+++ swea/d4/S_햄스터.java
@@ -5,8 +5,8 @@ import java.util.*;
 
 public class S_햄스터 {
 
-    static int N, X, M;
-    static int[] answer, check;
+    static int N, X, M, hamsterCnt;
+    static int[] answer, hamsters;
     static int[][] record;
 
     static StringTokenizer st;
@@ -17,56 +17,47 @@ public class S_햄스터 {
         int T = Integer.parseInt(br.readLine());
         for (int tc = 1; tc <= T; tc++) {
             init();
+            dupPerm(0);
 
             sb.append("#").append(tc).append(" ");
-            process(0);
-
-            if (answer[0] == -1) sb.append(-1);
-            else for (int i = 0; i < answer.length; i++) sb.append(answer[i]).append(" ");
+            if (hamsterCnt == -1) sb.append(-1);
+            else {
+                for (int i : answer) {
+                    sb.append(i).append(" ");
+                }
+            }
             sb.append("\n");
         }
 
         System.out.println(sb);
     }
 
-    static void process(int cnt) {
+    static void dupPerm(int cnt) {
+        if (!isAvailable(cnt - 1)) return;
 
         if (cnt == N) {
-            
-            if (!isAvailable()) return;
-
-            int maxSum = 0;
-            int nowSum = 0;
-            for (int i = 0; i < answer.length; i++) {
-                maxSum += answer[i];
-                nowSum += check[i];
-            }
-
-            if (nowSum > maxSum) {
-                for (int i = 0; i < answer.length; i++) {
-                    answer[i] = check[i];
-                }
-            }
+            countHamster();
 
             return;
         }
 
         for (int i = 0; i <= X; i++) {
-
-            check[cnt] = i;
-            process(cnt + 1);
+            hamsters[cnt] = i;
+            dupPerm(cnt + 1);
         }
     }
 
-    static boolean isAvailable() {
-        for (int i = 0; i < record.length; i++) {
+    static boolean isAvailable(int num) {
+        for (int i = 0; i < M; i++) {
             int l = record[i][0] - 1;
             int r = record[i][1] - 1;
             int s = record[i][2];
 
+            if (num != r) continue;
+
             int sum = 0;
             for (int j = l; j <= r; j++) {
-                sum += check[j];
+                sum += hamsters[j];
             }
 
             if (sum != s) return false;
@@ -75,15 +66,28 @@ public class S_햄스터 {
         return true;
     }
 
+    static void countHamster() {
+        int sum = 0;
+        for (int hamster : hamsters) {
+            sum += hamster;
+        }
+
+        if (sum > hamsterCnt) {
+            System.arraycopy(hamsters, 0, answer, 0, answer.length);
+            hamsterCnt = sum;
+        }
+    }
+
     static void init() throws IOException {
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         X = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
+        hamsterCnt = -1;
+
         answer = new int[N];
-        Arrays.fill(answer, -1);
-        check = new int[N];
+        hamsters = new int[N];
 
         record = new int[M][3];
         for (int i = 0; i < M; i++) {

44048a05f8eec7b6e09d0956e76b8d7eb0b81618
daegyu-song <sdk0109@naver.com>
2026-02-16 17:38:39 +0900

    swea_벌꿀채취

--- swea/S_벌꿀채취.java
+++ swea/S_벌꿀채취.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class S_벌꿀채취 {
 
-    static int N, M, C, max;
-    static int[][] field, maxField;
+    static int N, M, C, maxRevenue;
+    static int[][] honey, revenue;
 
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
@@ -12,36 +12,36 @@ public class S_벌꿀채취 {
 
     public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(br.readLine());
-
         for (int tc = 1; tc <= T; tc++) {
             init();
-            createMaxField();
-            process();
-            sb.append("#").append(tc).append(" ").append(max).append("\n");
+            createMoneyArr();
+            combination();
+
+            sb.append("#").append(tc).append(" ").append(maxRevenue).append("\n");
         }
 
         System.out.println(sb);
     }
 
-    static void process() {
-        int first = 0, second = 0, start = 0;
+    static void combination() {
+        int first = 0, second = 0;
 
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N - M + 1; j++) {
-                first = maxField[i][j];
+                first= revenue[i][j];
 
                 for (int q = i; q < N; q++) {
-                    start = q == i ? j + M : 0;
+                    int start = q == i ? j + M : 0;
                     for (int k = start; k < N - M + 1; k++) {
-                        second = maxField[q][k];
-                        max = Math.max(max, first + second);
+                        second = revenue[q][k];
+                        maxRevenue = Math.max(maxRevenue, first + second);
                     }
                 }
             }
         }
     }
 
-    static void createMaxField() {
+    static void createMoneyArr() {
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N - M + 1; j++) {
                 subset(i, j, 0, 0, 0);
@@ -50,34 +50,34 @@ public class S_벌꿀채취 {
     }
 
     static void subset(int i, int j, int cnt, int sum, int mul) {
-
         if (sum > C) return;
 
         if (cnt == M) {
-            maxField[i][j - M] = Math.max(maxField[i][j - M], mul);
+            revenue[i][j - M] = Math.max(revenue[i][j - M], mul);
             return;
         }
 
-        subset(i, j + 1, cnt + 1, sum + field[i][j], mul + field[i][j] * field[i][j]);
+        subset(i, j + 1, cnt + 1, sum + honey[i][j], mul + honey[i][j] * honey[i][j]);
 
         subset(i, j + 1, cnt + 1, sum, mul);
     }
 
-    static void init() throws IOException {
+    static void init() throws IOException{
         st = new StringTokenizer(br.readLine());
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
         C = Integer.parseInt(st.nextToken());
 
-        field = new int[N][N];
+        honey = new int[N][N];
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0; j < N; j++) {
-                field[i][j] = Integer.parseInt(st.nextToken());
+                honey[i][j] = Integer.parseInt(st.nextToken());
             }
         }
 
-        max = Integer.MIN_VALUE;
-        maxField = new int[N][N - M + 1];
+        revenue = new int[N][N - M + 1];
+
+        maxRevenue = Integer.MIN_VALUE;
     }
 }

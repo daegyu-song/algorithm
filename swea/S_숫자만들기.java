7dc6d04be87b50f834fe8740ccfcadef7c3c0781
daegyu-song <sdk0109@naver.com>
2026-02-16 22:23:28 +0900

    swea_숫자만들기

--- swea/S_숫자만들기.java
+++ swea/S_숫자만들기.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class S_숫자만들기 {
 
-    static int N, max, min;
-    static int[] nums, operators;
+    static int N, min, max;
+    static int[] operators, numbers;
 
     static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
@@ -12,10 +12,9 @@ public class S_숫자만들기 {
 
     public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(br.readLine());
-
         for (int tc = 1; tc <= T; tc++) {
             init();
-            process(1, nums[0]);
+            countingPerm(0, numbers[0]);
 
             sb.append("#").append(tc).append(" ").append(max - min).append("\n");
         }
@@ -23,11 +22,10 @@ public class S_숫자만들기 {
         System.out.println(sb);
     }
 
-    static void process(int idx, int value) {
-
-        if (idx == N) {
-            max = Math.max(max, value);
-            min = Math.min(min, value);
+    static void countingPerm(int cnt, int result) {
+        if (cnt == N - 1) {
+            max = Math.max(max, result);
+            min = Math.min(min, result);
             return;
         }
 
@@ -35,14 +33,16 @@ public class S_숫자만들기 {
             if (operators[i] == 0) continue;
 
             operators[i]--;
-
-            if (i == 0) process(idx + 1, value + nums[idx]);
-            else if (i == 1) process(idx + 1, value - nums[idx]);
-            else if (i == 2) process(idx + 1, value * nums[idx]);
-            else process(idx + 1, value / nums[idx]);
-
+            countingPerm(cnt + 1, calculate(result, numbers[cnt + 1], i));
             operators[i]++;
         }
+    }
+
+    static int calculate(int now, int next, int operator) {
+        if (operator == 0) return now + next;
+        else if (operator == 1) return now - next;
+        else if (operator == 2) return now * next;
+        else return now / next;
     }
 
     static void init() throws IOException {
@@ -54,13 +54,13 @@ public class S_숫자만들기 {
             operators[i] = Integer.parseInt(st.nextToken());
         }
 
-        nums = new int[N];
+        numbers = new int[N];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < nums.length; i++) {
-            nums[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < numbers.length; i++) {
+            numbers[i] = Integer.parseInt(st.nextToken());
         }
 
-        max = Integer.MIN_VALUE;
         min = Integer.MAX_VALUE;
+        max = Integer.MIN_VALUE;
     }
 }

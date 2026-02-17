56f14d7f10c5235aba7c66f08fba05c254ba121b
daegyu-song <sdk0109@naver.com>
2026-02-17 22:49:06 +0900

    swea_d3_NQueen

--- swea/d3/S_NQueen.java
+++ swea/d3/S_NQueen.java
@@ -5,49 +5,46 @@ import java.util.*;
 
 public class S_NQueen {
 
-    static int N, totalCnt;
-    static boolean[] col, slash, bSlash;
+    static int N, cnt;
+    static boolean[] column, slash, bSlash;
 
-    static StringTokenizer st;
     static StringBuilder sb = new StringBuilder();
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
     public static void main(String[] args) throws IOException {
         int T = Integer.parseInt(br.readLine());
-
         for (int tc = 1; tc <= T; tc++) {
             init();
-            setQueen(0);
+            process(0);
 
-            sb.append("#").append(tc).append(" ").append(totalCnt).append("\n");
+            sb.append("#").append(tc).append(" ").append(cnt).append("\n");
         }
 
         System.out.println(sb);
     }
 
-    static void setQueen(int row) {
-
-        if (row > N - 1) {
-            totalCnt++;
+    static void process(int row) {
+        if (row == N) {
+            cnt++;
             return;
         }
 
         for (int c = 0; c < N; c++) {
-            if (col[c] || slash[row + c] || bSlash[(row - c) + N - 1]) continue;
+            if (column[c] || slash[row + c] || bSlash[row - c + N - 1]) continue;
 
-            col[c] = slash[row + c] = bSlash[(row - c) + N - 1] = true;
-            setQueen(row + 1);
-            col[c] = slash[row + c] = bSlash[(row - c) + N - 1] = false;
+            column[c] = slash[row + c] = bSlash[row - c + N - 1] = true;
+            process(row + 1);
+            column[c] = slash[row + c] = bSlash[row - c + N - 1] = false;
         }
     }
 
     static void init() throws IOException {
         N = Integer.parseInt(br.readLine());
 
-        totalCnt = 0;
-
-        col = new boolean[N];
+        column = new boolean[N];
         slash = new boolean[2 * N - 1];
         bSlash = new boolean[2 * N - 1];
+
+        cnt = 0;
     }
 }

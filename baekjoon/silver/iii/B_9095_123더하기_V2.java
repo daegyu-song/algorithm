b5cfe4abdc42990eb2d35aff2a9482f30fe7e349
daegyu-song <sdk0109@naver.com>
2026-03-22 22:02:49 +0900

    baekjoon_silver_9095_123더하기 [DP]

---
+++ baekjoon/silver/iii/B_9095_123더하기_V2.java
@@ -0,0 +1,36 @@
+package silver.iii;
+
+import java.io.*;
+
+public class B_9095_123더하기_V2 {
+
+    static int[] dp;
+
+    static StringBuilder sb = new StringBuilder();
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        int T = Integer.parseInt(br.readLine());
+        preprocess();
+        while (T-- > 0) {
+            initAndProcess();
+        }
+
+        System.out.println(sb);
+    }
+
+    static void preprocess() {
+        dp = new int[11];
+        dp[1] = 1;
+        dp[2] = 2;
+        dp[3] = 4;
+
+        for (int i = 3; i < dp.length; i++) {
+            dp[i] = dp[i - 3] + dp[i - 2] + dp[i - 1];
+        }
+    }
+
+    static void initAndProcess() throws IOException {
+        sb.append(dp[Integer.parseInt(br.readLine())]).append("\n");
+    }
+}

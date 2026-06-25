cc88d8f50ce2810652bfab53b5405c24895983c5
daegyu-song <141833286+daegyu-song@users.noreply.github.com>
2026-06-25 22:42:43 +0900

    [Passed] 최고의 33위치 (98ms, 9MB)

--- codetree/trail4/최고의 33위치/best-place-of-33.java
+++ codetree/trail4/최고의 33위치/best-place-of-33.java
@@ -2,12 +2,16 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
+
+    static int N;
+    static int[][] arr;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         
         int max = 0;
-        int N = Integer.parseInt(br.readLine());
-        int[][] arr = new int[N][N];
+        N = Integer.parseInt(br.readLine());
+        arr = new int[N][N];
 
         for (int r = 0; r < arr.length; r++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
@@ -18,17 +22,21 @@ public class Main {
         
         for (int r = 0; r <= N - 3; r++) {
             for (int c = 0; c <= N - 3; c++) {
-                int num = 0;
-                for (int i = 0; i < 3; i++) {
-                    for (int j = 0; j < 3; j++) {
-                        num += arr[r + i][c + j];
-                    }
-                }
-
-                max = Math.max(max, num);
+                max = Math.max(max, getNumOfGold(r, r + 2, c, c + 2));
             }
         }
 
         System.out.println(max);
+    }
+
+    static int getNumOfGold(int rs, int re, int cs, int ce) {
+        int numOfGold = 0;
+        for (int r = rs; r <= re; r++) {
+            for (int c = cs; c <= ce; c++) {
+                numOfGold += arr[r][c];
+            }
+        }
+
+        return numOfGold;
     }
 }

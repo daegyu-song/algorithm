e8a27e587475aee622b3e4020c3a61a92d7d3830
daegyu-song <141833286+daegyu-song@users.noreply.github.com>
2026-06-27 22:26:51 +0900

    [Passed] 트로미노 (227ms, 11MB)

--- codetree/trail4/트로미노/tromino.java
+++ codetree/trail4/트로미노/tromino.java
@@ -5,9 +5,31 @@ public class Main {
 
     static int answer, N, M;
     static int[][] arr;
-    static int[][] s = {{0, 0}, {0, 0}, {0, 0}, {0, 1}, {0, 0}, {0, 0}};
-    static int[][] f = {{1, 0}, {0, 1}, {0, 1}, {1, 1}, {0, 1}, {1, 0}};
-    static int[][] u = {{1, 1}, {1, 0}, {1, 1}, {1, 0}, {0, 2}, {2, 0}};
+    static int[][][] shapes = new int[][][] {
+        {{1, 1, 0},
+        {1, 0, 0},
+        {0, 0, 0}},
+
+        {{1, 1, 0},
+        {0, 1, 0},
+        {0, 0, 0}},
+
+        {{1, 0, 0},
+        {1, 1, 0},
+        {0, 0, 0}},
+
+        {{0, 1, 0},
+        {1, 1, 0},
+        {0, 0, 0}},
+
+        {{1, 1, 1},
+        {0, 0, 0},
+        {0, 0, 0}},
+
+        {{1, 0, 0},
+        {1, 0, 0},
+        {1, 0, 0}}
+    };
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -27,21 +49,33 @@ public class Main {
         for (int r = 0; r < N; r++) {
             for (int c = 0; c < M; c++) {
                 for (int i = 0; i < 6; i++) {
-                    int sum = 0;
-
-                    if (r + s[i][0] >= N || c + s[i][1] >= M) continue;
-                    if (r + f[i][0] >= N || c + f[i][1] >= M) continue;
-                    if (r + u[i][0] >= N || c + u[i][1] >= M) continue;
-
-                    sum += arr[r + s[i][0]][c + s[i][1]];
-                    sum += arr[r + f[i][0]][c + f[i][1]];
-                    sum += arr[r + u[i][0]][c + u[i][1]];
-
-                    answer = Math.max(answer, sum);
+                    answer = Math.max(answer, getMaxSum(r, c));
                 }
             }
         }
 
         System.out.println(answer);
+    }
+
+    static int getMaxSum(int r, int c) {
+        int maxSum = 0;
+
+        for (int i = 0; i < 6; i++) {
+            int sum = 0;
+            boolean isPossible = true;
+            for (int dr = 0; dr < 3; dr++) {
+                for (int dc = 0; dc < 3; dc++) {
+                    if (shapes[i][dr][dc] == 0) continue;
+                    if (r + dr >= N || c + dc >= M) isPossible = false;
+                    else sum += arr[r + dr][c + dc]; 
+                }
+            }
+
+            if (isPossible) {
+                maxSum = Math.max(maxSum, sum);
+            }
+        }
+
+        return maxSum;
     }
 }

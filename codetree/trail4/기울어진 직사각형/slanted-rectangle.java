a5d393e645cc1250eafbf3105336ba9894738597
daegyu-song <141833286+daegyu-song@users.noreply.github.com>
2026-06-29 14:52:13 +0900

    [Passed] 기울어진 직사각형 (118ms, 11MB)

--- codetree/trail4/기울어진 직사각형/slanted-rectangle.java
+++ codetree/trail4/기울어진 직사각형/slanted-rectangle.java
@@ -23,45 +23,31 @@ public class Main {
 
         for (int r = 0; r < N; r++) {
             for (int c = 0; c < N; c++) {
-                answer = Math.max(answer, getMaxSum(r, c));
+                for (int k = 1; k < N; k++) {
+                    for (int l = 1; l < N; l++) {
+                        answer = Math.max(answer, getMaxSum(r, c, k, l));
+                    }
+                }
             }
         }
 
         System.out.println(answer);
     }
 
-    static int getMaxSum(int r, int c) {
+    static int getMaxSum(int r, int c, int k, int l) {
         int maxSum = 0;
+        int[] moveNum = {k, l, k, l};
 
-        for (int i = 1; i < N - 1; i++) {
-            for (int j = 1; j < N - i; j++) {
-                boolean isPossible = true;
-                int nr = r, nc = c, sum = 0;
-                
-                for (int d = 0; d < 4; d++) {
-                    int cnt = 0;
-                    if (d % 2 == 0) cnt = i;
-                    else cnt = j;
+        for (int d = 0; d < 4; d++) {
+            for (int q = 0; q < moveNum[d]; q++) {
+                r += dr[d];
+                c += dc[d];
 
-                    for (int k = 0; k < cnt; k++) {
-                        nr += dr[d];
-                        nc += dc[d];
-
-                        if (nr < 0 || nc < 0 || nr >= N || nc >= N) {
-                            isPossible = false;
-                            break;
-                        }
-
-                        sum += arr[nr][nc];
-                    }
-
-                    if (!isPossible) {
-                        sum = 0;
-                        break;
-                    }
+                if (r < 0 || c < 0 || r >= N || c >= N) {
+                    return 0;
                 }
 
-                maxSum = Math.max(maxSum, sum);
+                maxSum += arr[r][c];
             }
         }
 
